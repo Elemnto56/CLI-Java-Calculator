18b5d3dbebcd6efb0a4d0439c39176854d856def
Jaylon Christy <jaylon_christy@yahoo.com>
2025-04-10 21:03:21 -0500

    Renamed HelloWorld.java to Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Cake {
+    public static String cakeMaker() {
+        return "Cake Made";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Cake myObj = new Cake();
+        System.out.println("Hello World");
+        System.out.println(Cake.cakeMaker());
+        System.out.println(myObj.cakeMaker());
+        Systebfjsdnfkjnfsdnfsdaj;nfaijdsa
+    }
+}
+
