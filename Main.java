516bdbe8689c60051be80e88bf5b712d5cb9e6e8
Jaylon Christy <jaylon_christy@yahoo.com>
2025-04-13 00:50:01 -0500

    Auto sync: Sun 13 Apr 00:50:01 CDT 2025

--- Main.java
+++ Main.java
@@ -3,15 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Type in a number:");
-        int a = sc.nextInt();
-
-        System.out.println("Number you typed:");
-        System.out.println(a);
-
-        sc.close();
+        
     }
 }
-
-// Edit lol
-// Push worked
