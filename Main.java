67df42ac238b2a729fcb52c346ba778a0e126d67
Jaylon Christy <jaylon_christy@yahoo.com>
2025-04-13 01:05:01 -0500

    Auto sync: Sun 13 Apr 01:05:01 CDT 2025

--- Main.java
+++ Main.java
@@ -5,17 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Addition!");
         
-        // Exception
-        try {
-            // First number
-            System.out.println("Give first number");
-            int userOne = sc.nextInt();
-        } catch (Exception e) {
-            System.out.println("Type a number");
+        int userOne = 0;
+        int userTwo = 0;
+
+        while (/* Insert */) {
+            // Exception
+            try {
+                // First number
+                System.out.println("Give first number");
+                userOne = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Type a number");
+            }
         }
         // Second number
         System.out.println("Give second number");
-        int userTwo = sc.nextInt();
+        userTwo = sc.nextInt();
 
         // Answer
         System.out.println(userOne + " + " + userTwo + " = " + (userOne + userTwo));
