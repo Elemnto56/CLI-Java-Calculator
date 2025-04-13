6d701bf146fb3f39713c9841284900e7df82ec00
Jaylon Christy <jaylon_christy@yahoo.com>
2025-04-13 00:55:01 -0500

    Auto sync: Sun 13 Apr 00:55:01 CDT 2025

--- Main.java
+++ Main.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Welcome to Addition!");
+
+        // First number
+        System.out.println("Give first number");
+        int userOne = sc.nextInt();
+
+        // Second number
+        System.out.println("Give second number");
+        int userTwo = sc.nextInt();
+
+        // Answer
+        System.out.println(userOne + " + " + userTwo + " = " + (userOne + userTwo));
     }
 }
