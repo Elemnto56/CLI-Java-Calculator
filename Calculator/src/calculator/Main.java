f32ebc5c50bc5f8fe9ad06f5e4df9ca1b912e54e
Jaylon Christy <132957328+Elemnto56@users.noreply.github.com>
2025-04-15 03:00:40 +0000

    Fixed soem files

--- Calculator/src/calculator/Main.java
+++ Calculator/src/calculator/Main.java
@@ -5,19 +5,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! Welcome to Calculator! Type a number for an operation below: ");
-        System.out.println("1. Addition \n" + "2. Multiplication");
+        System.out.println("1. Addition \n" + "2. Subtraction \n" + "3. Multiplication \n" + "4. Division");
+        
+        boolean validInput = false;
 
-        int userInput = sc.nextInt();
+        while (!validInput) {
+            try {
+                int userInput = sc.nextInt();
+                validInput = true;
+                switch (userInput) {
+                    case 1:
+                        Addition.main(args);
+                        break;
+                    case 2:
+                        Subtraction.main(args);
+                        break;
+                    case 3:
+                        Multiplication.main(args);
+                        break;
+                    case 4:
+                        Division.main(args);
+                        break;
+                    default:
+                        System.out.println("Invalid input");
+                }
 
-        switch (userInput) {
-            case 1:
-                Addition.main(args);
-                break;
-            case 2:
-                Multiplication.main(args);
-                break;
+            } catch (Exception e) {
+                sc.nextLine();
+                System.out.println("Input a number, not a letter or whatever that was");      
+            }
         }
-
-        sc.close();
+            sc.close();
     }
 }
