da83a91bfc5a795d1e2f7ff9ed118fa5f8bb1165
Jaylon Christy <jaylon_christy@yahoo.com>
2025-04-15 20:20:02 -0500

    Auto sync: Tue 15 Apr 20:20:02 CDT 2025

--- Calculator/src/calculator/Exp4jExample.java
+++ Calculator/src/calculator/Exp4jExample.java
@@ -3,7 +3,17 @@ package calculator;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 import java.util.Scanner;
+import org.apache.commons.math3.fraction.Fraction;
 
+class FractionTest {
+    public static void main(String[] args) {
+        Fraction f = new Fraction(5, 8);
+        System.out.println("Fraction: " + f);         // ➤ Fraction: 5/8
+        System.out.println("As double: " + f.doubleValue());  // ➤ As double: 0.625
+    }
+}
+
+/* 
 public class Exp4jExample {
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
