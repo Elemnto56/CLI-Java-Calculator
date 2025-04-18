336cb9a5921feafff30cc4173f1de48589bf8d3e
Jaylon Christy <132957328+Elemnto56@users.noreply.github.com>
2025-04-18 02:37:32 +0000

    Fractions

--- Calculator/src/calculator/Fractions.java
+++ Calculator/src/calculator/Fractions.java
@@ -1,19 +1,26 @@
 package calculator;
 import org.apache.commons.math3.fraction.BigFraction;
+
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Fractions {
     static Scanner sc = new Scanner(System.in);
 
-    public static void addFrac() {
+    public static void Fracmaker() {
         System.out.println("Type out first Fraction (ex: 3/2): ");
         String userFrac = sc.nextLine();
         
-        BigFraction frac = new BigFraction(userFrac);
+        String[] fracStrings = userFrac.trim().split("/");
+
+        BigInteger numerator = new BigInteger(fracStrings[0].trim());
+        BigInteger denominator = new BigInteger(fracStrings[1].trim());
+
+        BigFraction frac = new BigFraction(numerator, denominator);
         System.out.println("Your fraction is: " + frac);
     }
 
     public static void main(String[] args) {
-        addFrac();
+        Fracmaker();
     }
 }
