1a4e5a0c46aaa5aa59fcc552e08d68994fcd205a
Jaylon Christy <jaylon_christy@yahoo.com>
2025-04-22 21:10:02 -0500

    Auto sync: Tue 22 Apr 21:10:02 CDT 2025

--- Calculator/src/calculator/misc/Dev_Room.java
+++ Calculator/src/calculator/misc/Dev_Room.java
@@ -5,6 +5,27 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Dev_Room {
+    public static void main(String[] args) {
+        System.out.println("""
+                Welcome to the Dev Room! Or, as the dev class it: "The Distillery"!
+                Pick a file to view:
+                [1] Addition.java
+                [2] Subtraction.java
+                [3] Multiplication.java
+                [4] Division.java
+                """);
+
+        us
+        switch () {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+    /* 
     public static void printFile(String fileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
@@ -15,4 +36,5 @@ public class Dev_Room {
             System.out.println("Oops! Could not read " + fileName);
         }
     }
+    */
 }
