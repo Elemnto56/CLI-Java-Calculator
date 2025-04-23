f4fc6b20a1c54f0b82029f8c0384363822463aaa
Jaylon Christy <jaylon_christy@yahoo.com>
2025-04-22 21:25:01 -0500

    Auto sync: Tue 22 Apr 21:25:01 CDT 2025

--- Calculator/src/calculator/misc/Dev_Room.java
+++ Calculator/src/calculator/misc/Dev_Room.java
@@ -9,7 +9,7 @@ public class Dev_Room {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("""
-                Welcome to the Dev Room! Or, as the dev class it: "The Distillery"!
+                Welcome to the Dev Room! Or, as the dev calls it: "The Distillery"!
                 Pick a file to view:
                 [1] Addition.java
                 [2] Subtraction.java
@@ -19,7 +19,7 @@ public class Dev_Room {
 
         int userInput = sc.nextInt();
         switch (userInput) {
-            case 1 -> printFile("calculator/Four_Function/Addition.java");
+            case 1 -> printFile("/home/pi/my-code-project/Calculator/src/calculator/Four_Function/Addition.java");
             case 2 -> printFile("calculator/Four_Function/Subtraction.java");
             case 3 -> printFile("calculator/Four_Function/Multiplication.java");
             case 4 -> printFile("calculator/Four_Function/Division.java");
